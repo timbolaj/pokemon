dffb427b342b9cde2ed2561c9149dbcc45a8963d
Aaron <timbolaj@gmail.com>
2020-07-23 07:37:53 -0400

    extracted info, still need to do an axios call on the species property

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -1,15 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 
 export default function Pokemon(props) {
   const [pokemonData, setPokemonData] = useState([]);
   const { name, url } = props
+
+  const extractAbilities = abilityObj => {
+    let abilityList = [];
+    abilityObj.abilities.forEach(ability => {
+      abilityList.push(ability.name)
+    })
+
+    return abilityList;
+  }
+
+  const extractSpecies = speciesObj => {
+
+  }
+
+  // const extractInfo = data => {
+  //   return {
+  //     id: data.id,
+  //     abilities: extractAbilities(data.abilities),
+  //     height: data.height,
+  //     moves: data.moves.length,
+  //     sprites: data.front_default,
+  //     types: extractTypes(data.types),
+  //     evolves_to: extractSpecies(data.species),
+  //   }
+  // }
   
   useEffect(() => {
-    Axios.get(url)
-      .then(res => console.log(res))
+    axios.get(url)
+      .then(res => {
+        setPokemonData(res.data)
+      })
       .catch(err => console.log("Err", err))
   }, [])
+
+  console.log(pokemonData);
 
   return (
     <div>{name}</div>
--- src/Components/PokemonList.js
+++ src/Components/PokemonList.js
@@ -4,26 +4,64 @@ const axios = require('axios');
 
 export default function PokemonList() {
   const [pokeData, setData] = useState([]);
+  const [pokeName, setName] = useState([]);
+
+  // function extractPredecessor(url, setData) {
+  //   axios.get(url)
+  //     .then(res => setData())
+  //   .catch(err => console.log(err, "Error"))
+  // }
+
+  const extractTypes = typesArr => {
+    let typesList = [];
+    typesArr.forEach(typ => {
+      typesList.push(typ.type.name)
+    })
+    return typesList;
+  }
+
+  const extractPokemon = (data, setData) => {
+    data.forEach(datum => {
+      axios.get(datum.url)
+        .then(res => {
+          setData( prev => ([ ...prev,
+            {
+              name: res.data.name,
+              height: res.data.height,
+              weight: res.data.weight,
+              id: res.data.id,
+              sprite: res.data.sprites.front_default,
+              types: extractTypes(res.data.types),
+              species: res.data.species.url
+            }
+          ]))
+        })
+    })
+  }
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=150`)
-      .then(res => setData(res.data.results))
+      .then(res => {
+        extractPokemon(res.data.results, setData)
+      })
       .catch(err => console.log("Err", err))
   }, [])
 
-  const pokemonData = pokeData.map(pokemon => {
+  // const pokemonData = pokeData.map(pokemon => {
 
-    return (
-      <Pokemon
-        name={pokemon.name}
-        url={pokemon.url}
-      />
-    )
-  })
+  //   return (
+  //     <Pokemon
+  //       name={pokemon.name}
+  //       url={pokemon.url}
+  //     />
+  //   )
+  // })
+
+  console.log(pokeData);
 
   return (
     <div>
-      {pokemonData}
+      {/* {pokemonData} */}
     </div>
   )
 }
