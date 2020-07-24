606ae939393f379d02b6e9b7588113e596b202a1
Aaron <timbolaj@gmail.com>
2020-07-24 10:23:38 -0400

    modified font size of nav title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../Styles/Home.scss';
+import axios from 'axios';
 
-export default function Home() {
+export default function Home(props) {
+
+  const { toggleMode } = props;
+
+  const [homeImage, setImage] = useState();
+
+  useEffect(() => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+      .then(res => {
+        return getImage(res.data.sprites.front_shiny, setImage);
+      })
+  }, [])
+  const getImage = (img, setImage) => {
+    return setImage(img);
+  }
+
   return (
-    <div>
-      Home
+    <div className='home'>
+      <h1>Welcome to the Pokédex</h1>
+      <p>A website that hosts a collection of pokemon-related information</p>
+      <img src={homeImage}/>
     </div>
   )
 }
